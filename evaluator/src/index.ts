5dc0c29b260796b8cae27328b22be620a7cfce8e
Luis Guzman <luis.guzman@falcani.com>
2023-09-14 12:42:28 -0500

    Add initial orchestration files

---
+++ evaluator/src/index.ts
@@ -0,0 +1,26 @@
+import http from 'node:http'
+import express, { Application } from 'express'
+
+const app: Application = express()
+const NODE_ENV = process.env.NODE_ENV
+const PORT = process.env.EVALUATOR_PORT
+
+if (!NODE_ENV || !PORT) {
+  console.error('FATAL: NODE_ENV or PORT are not set')
+  console.log(`NODE_ENV ${NODE_ENV}`)
+  console.log(`PORT ${PORT}`)
+  process.exit(1)
+}
+
+const server = http.createServer(app)
+
+server
+  .listen(PORT)
+  .on('listening', () => {
+    console.log(`Running in ${NODE_ENV.toUpperCase()} mode`)
+    console.log(`Listening on port: ${PORT}`)
+  })
+  .on('error', (err: unknown) => {
+    console.error(err)
+    process.exit(1)
+  })
