c293b86f0235d0d90d8ec564c2f7c06a60f2eaef
Alonso Cantu <a.cantu@uniandes.edu.co>
2023-09-15 17:42:50 -0600

    Change the token time expiration

--- authorizer/src/utils/utils.ts
+++ authorizer/src/utils/utils.ts
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 import jwt_decode from "jwt-decode";
 
 export async function generateAccessToken(email: string): Promise<string> {
-    const accessToken = jwt.sign({email: email }, process.env.TOKEN_SECRET, { expiresIn: 2});
+    const accessToken = jwt.sign({email: email }, process.env.TOKEN_SECRET, { expiresIn: "2d"});
     return accessToken;
 }
 
