d3cb9e5d8716a3c35abde355601cf36eeb777ef4
Luis Guzman <luis.guzman@falcani.com>
2023-09-16 12:56:03 -0500

    Add create project form

--- front-web/src/app/dashboard/projects/page.tsx
+++ front-web/src/app/dashboard/projects/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const people = [
@@ -33,14 +34,15 @@ const people = [
 export default function ProjectsPage() {
   return (
     <div className='p-8 mx-auto max-w-2xl'>
-      <button
+      <Link
+        href='/dashboard/projects/create'
         type='button'
         className='px-3 relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
       >
         <span className='absolute -inset-1.5' />
         <span className='sr-only'>View notifications</span>
         Add Project
-      </button>
+      </Link>
 
       <ul role='list' className='divide-y divide-gray-100'>
         {people.map((person) => (
