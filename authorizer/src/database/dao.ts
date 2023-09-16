d8ba174fc0bdd11c4c55adebd560584b70beb5fa
Luis Guzman <luis.guzman@falcani.com>
2023-09-16 11:09:12 -0500

    Fix authorizer

--- authorizer/src/database/dao.ts
+++ authorizer/src/database/dao.ts
@@ -1,133 +1,96 @@
-const { Client } = require('pg');
-const bind  = require('pg-bind');
-
+const { Client } = require('pg')
+const bind = require('pg-bind')
 
 class Dao {
-    private client: any;
+  private client
 
-    constructor() {
-        this.client = new Client({
-            user: 'postgres',
-            host: 'localhost',
-            database: 'postgres',
-            password: 'postgres',
-            port: 5432,
-        });
-        this.client.connect();
-    }
+  constructor() {
+    this.client = new Client({
+      user: process.env.POSTGRES_USER,
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB_NAME,
+      password: process.env.POSTGRES_PASSWORD,
+      port: process.env.POSTGRES_PORT,
+    })
+    this.client.connect()
+  }
 
-    async createUser(email: string, firstname: string, lastname: string, age: number) {
-        console.log(email);
-        const tableAvailable = await this.isTableOk();
+  async authenticateUser(
+    personId: number,
+    country: string,
+    lenguages: string,
+    academicalDataId: number,
+    technicalDataId: number,
+    workDataId: number,
+    isAvailable: boolean,
+    softSkills: string,
+    interviewId: number,
+    email: string,
+    password: string,
+    token: string,
+  ) {
+    // Generate token
 
-        if (tableAvailable) {
-            console.log(tableAvailable);
-
-            const query =
-                `INSERT INTO users ("email", "firstname", "lastname", "age") VALUES (:email, :firstname, :lastname, :age)`;
-
-            const queryPrepared = bind(query, {
-                email: email,
-                firstname: firstname,
-                lastname: lastname,
-                age: age
-            });
-
-            try {
-                const res = await this.client.query(queryPrepared);
-                console.log('User successfully created');
-            } catch (err) {
-                console.log(err);
-            }
-        } else {
-            return {msg: "400"}
-        }
-
-    }
-
-    async isTableOk() {
-
-        const query = `
-    SELECT EXISTS(SELECT 1 FROM pg_tables WHERE tablename = 'users') AS table_existence
-    `;
-        let res: any;
-        try {
-            res = await this.client.query(query);
-            console.log('isTableOk validation executed');
-        } catch (err) {
-            console.log(err);
-        }
-        const tableExistence = res['rows'][0]['table_existence'];
-        return tableExistence;
-    }
-
-    async authenticateUser(personId: number, country: string, lenguages: string, academicalDataId: number, technicalDataId: number, workDataId: number, isAvailable: boolean, softSkills: string, interviewId: number, email: string, password: string, token: string) {
-
-        // Generate token
-
-        // Insert the information
-        const query = `INSERT INTO "Candidate" ("personId", "country", "lenguages", "academicalDataId", "technicalDataId", 
+    // Insert the information
+    const query = `INSERT INTO "Candidate" ("personId", "country", "lenguages", "academicalDataId", "technicalDataId", 
         "workDataId", "isAvailable", "softSkills", "interviewId", "email", "password", "token")
         VALUES (:personId, :country, :lenguages, :academicalDataId, :technicalDataId,
-         :workDataId, :isAvailable, :softSkills, :interviewId, :email, :password, :token)`;
+         :workDataId, :isAvailable, :softSkills, :interviewId, :email, :password, :token)`
 
-            const queryPrepared = bind(query, {
-                personId: personId,
-                country: country,
-                lenguages: lenguages,
-                academicalDataId: academicalDataId,
-                technicalDataId: technicalDataId,
-                workDataId: workDataId,
-                isAvailable: isAvailable,
-                softSkills: softSkills,
-                interviewId: interviewId,
-                email: email,
-                password: password,
-                token: token
-            });
+    const queryPrepared = bind(query, {
+      personId: personId,
+      country: country,
+      lenguages: lenguages,
+      academicalDataId: academicalDataId,
+      technicalDataId: technicalDataId,
+      workDataId: workDataId,
+      isAvailable: isAvailable,
+      softSkills: softSkills,
+      interviewId: interviewId,
+      email: email,
+      password: password,
+      token: token,
+    })
 
-            try {
-                const res = await this.client.query(queryPrepared);
-                console.log('Token update');
-                return {msg: "201"};
-            } catch (err) {
-                console.log(err);
-            }
-            return {msg: "400"}
-        }
-
-    async getUserInfo(email: string) {
-
-        const query = `SELECT *  FROM  "Candidate" WHERE email = :email`;
-
-        const queryPrepared = bind(query, {
-            email: email,
-        });
-
-        try {
-            const res = await this.client.query(queryPrepared);
-            console.log('Email information');
-            const info = {
-                personId: res.rows[0]['personId'],
-                country: res.rows[0]['country'],
-                lenguages: res.rows[0]['lenguages'],
-                academicalDataId: res.rows[0]['academicalDataId'],
-                technicalDataId: res.rows[0]['technicalDataId'],
-                workDataId: res.rows[0]['workDataId'],
-                isAvailable: res.rows[0]['isAvailable'],
-                softSkills: res.rows[0]['softSkills'],
-                interviewId: res.rows[0]['interviewId'],
-                email: res.rows[0]['email'],
-                password: res.rows[0]['password']
-            }
-            return info;
-        } catch (err) {
-            console.log(err);
-        }
-        return {msg: "400"}
+    try {
+      const res = await this.client.query(queryPrepared)
+      console.log('Token update')
+      return { msg: '201' }
+    } catch (err) {
+      console.log(err)
     }
-};
-export default Dao;
+    return { msg: '400' }
+  }
 
+  async getUserInfo(email: string) {
+    const query = `SELECT *  FROM  "Candidate" WHERE email = :email`
 
+    const queryPrepared = bind(query, {
+      email: email,
+    })
 
+    try {
+      const res = await this.client.query(queryPrepared)
+      console.log('Email information')
+      const info = {
+        personId: res.rows[0].personId,
+        country: res.rows[0].country,
+        lenguages: res.rows[0].lenguages,
+        academicalDataId: res.rows[0].academicalDataId,
+        technicalDataId: res.rows[0].technicalDataId,
+        workDataId: res.rows[0].workDataId,
+        isAvailable: res.rows[0].isAvailable,
+        softSkills: res.rows[0].softSkills,
+        interviewId: res.rows[0].interviewId,
+        email: res.rows[0].email,
+        password: res.rows[0].password,
+      }
+      return info
+    } catch (err) {
+      console.log(err)
+    }
+    return { msg: '400' }
+  }
+}
+
+export default Dao
--- authorizer/src/index.ts
+++ authorizer/src/index.ts
@@ -1,14 +1,11 @@
-import http from 'node:http'
-import express, {Express} from 'express'
-import dotenv from 'dotenv';
-import cors from 'cors';
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express, { Express } from 'express'
+import routes from './routes/routes'
 
-dotenv.config();
+dotenv.config()
 
-// Import routes
-const routes     = require("./routes/routes")
-
-const app: Express = express();
+const app: Express = express()
 const NODE_ENV = process.env.NODE_ENV
 const PORT = process.env.AUTH_PORT
 
@@ -20,26 +17,28 @@ if (!NODE_ENV || !PORT) {
 }
 
 // For json data
-app.use(express.json());
-
+app.use(express.json())
 
 // Set CORS
-const allowedOrigins = ['http://localhost:3000', "http://localhost:8080"];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:8080']
 const options: cors.CorsOptions = {
-    origin: allowedOrigins
-};
-app.use(cors(options));
+  origin: allowedOrigins,
+}
+app.use(cors(options))
 
-// Handling routes requessts
-app.use("/", routes);
-app.use("/auth", routes);
-app.use("/auth/me", routes);
-app.use("/auth/ping", routes);
+// Handling routes requests
+app.use('/', routes)
+app.use('/auth', routes)
+app.use('/auth/me', routes)
+app.use('/auth/ping', routes)
 
-app.listen(PORT).on('listening', () => {
+app
+  .listen(PORT)
+  .on('listening', () => {
     console.log(`Running in ${NODE_ENV.toUpperCase()} mode`)
     console.log(`Listening on port: ${PORT}`)
-  }).on('error', (err: unknown) => {
+  })
+  .on('error', (err: unknown) => {
     console.error(err)
     process.exit(1)
   })
