8ffee352622e63394fe3a3d5b6162a4442b74ff2
Luis Guzman <luis.guzman@falcani.com>
2023-09-16 17:20:07 -0500

    Add company for candidate registration

--- front-web/src/app/dashboard/complete-signup-candidate/page.tsx
+++ front-web/src/app/dashboard/complete-signup-candidate/page.tsx
@@ -402,6 +402,27 @@ export default function CompleteSignUpPage() {
               </div>
             </div>
 
+            <div className='sm:col-span-4'>
+              <label
+                htmlFor='certifications'
+                className='block text-sm font-medium leading-6 text-gray-900'
+              >
+                Company
+              </label>
+              <div className='mt-2'>
+                <div className='flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'>
+                  <input
+                    type='text'
+                    name='certifications'
+                    id='certifications'
+                    autoComplete='certifications'
+                    className='block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
+                    placeholder='ABC Jobs'
+                  />
+                </div>
+              </div>
+            </div>
+
             <div className='sm:col-span-3'>
               <label
                 htmlFor='location'
