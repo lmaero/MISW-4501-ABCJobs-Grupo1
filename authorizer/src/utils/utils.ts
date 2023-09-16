d2a5ebd88ee6e78b9a83834372d34171908c67e7
Luis Guzman <luis.guzman@falcani.com>
2023-09-16 11:15:34 -0500

    Fix imports

--- authorizer/src/utils/utils.ts
+++ authorizer/src/utils/utils.ts
@@ -1,16 +1,16 @@
-const jwt = require('jsonwebtoken')
+import jwt, { JsonWebTokenError } from 'jsonwebtoken'
 import jwt_decode from 'jwt-decode'
 
 export async function generateAccessToken(email: string): Promise<string> {
-  const accessToken = jwt.sign({ email: email }, process.env.TOKEN_SECRET, {
+  if (!process.env.TOKEN_SECRET) throw JsonWebTokenError
+
+  return jwt.sign({ email: email }, process.env.TOKEN_SECRET, {
     expiresIn: '24h',
   })
-  return accessToken
 }
 
 export async function decodeToken(token: string): Promise<string> {
-  const dataDecoded: any = jwt_decode(token)
-  return dataDecoded
+  return jwt_decode(token)
 }
 
 export async function tokenExpired(expiredTime: number) {
