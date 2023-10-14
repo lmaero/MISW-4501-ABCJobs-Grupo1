cc5ec6005e0f75e7b1a26689652dd8f8c7b36e67
Alonso Cantu <a.cantu@uniandes.edu.co>
2023-10-14 14:12:41 -0600

    Login page tests and CI

---
+++ authorizer/src/__tests__/utils.test.ts
@@ -0,0 +1,44 @@
+import {decodeToken, generateAccessToken, tokenExpired} from "../utils/utils";
+import jwt_decode from "jwt-decode";
+import {JsonWebTokenError} from "jsonwebtoken";
+
+jest.mock("jwt-decode");
+
+describe("utils functions validation", () => {
+
+    test("Token expired", async () => {
+
+        const expected = await tokenExpired(1697306132);
+        const result = true;
+        expect(result).toStrictEqual(expected);
+    });
+
+    test("Token did not expired", async () => {
+
+        const expected = await tokenExpired(1697306132000);
+        const result = false;
+        expect(result).toStrictEqual(expected);
+    });
+
+    test("Access token generated", async () => {
+
+        const JsonWebTokenError = jest.mock('jsonwebtoken', () => ({ default: jest.fn(() => ({})) }));
+        const token = "12345"
+        try {
+            const expected = await generateAccessToken(token);
+        } catch(e) {
+            const error: any = e;
+            expect(error["name"]).toBe("JsonWebTokenError")
+        }
+
+    });
+
+    test("Decode token generated", async () => {
+
+        const jwtDecodeMock = jest.mock('jwt-decode', () => ({ default: jest.fn(() => ({})) }));
+        const token = "1234512345123451234512345123451234512345123451234512345123451234512345"
+        const expected = await decodeToken(token);
+        expect(jwtDecodeMock).toBeDefined();
+    });
+
+});
