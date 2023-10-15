7b4c0cbee4237ff72c8b8640e90b88d1fcfcec74
Luis Guzman <luis.guzman@falcani.com>
2023-10-15 10:02:07 -0500

    Configure Cypress and add base tests

---
+++ web/src/app/candidate/register/pageCandidateRegisterPage.cy.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CandidateRegisterPage from './page'
+
+describe('<CandidateRegisterPage />', () => {
+  it('renders', () => {
+    cy.mount(<CandidateRegisterPage />)
+  })
+})
