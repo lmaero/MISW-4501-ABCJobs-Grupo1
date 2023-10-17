cb30d4223a8f65d64691cff663f5ecf6b468cb17
Camilo Gálvez Vidal <c.galvezv@uniandes.edu.co>
2023-10-16 23:45:05 -0300

    feat(company-ms): add main behavior to register a new company

---
+++ company/src/routes/company.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import companyController from "../controllers/company";
+import { userValidation } from "../middlewares/auth";
+
+const companyRouter = express.Router();
+
+companyRouter.post("/register", userValidation, companyController.register);
+
+export default companyRouter;
