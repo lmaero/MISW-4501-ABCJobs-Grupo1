9c243d2c83e0e8c7af549bc9a22a23959f24d217
Luis Guzman <luis.guzman@falcani.com>
2023-10-17 17:59:54 -0500

    Lint and format with biome

--- authorizer/__tests__/authorizer.database.dao.test.ts
+++ authorizer/__tests__/authorizer.database.dao.test.ts
@@ -1,55 +1,55 @@
-import {decodeToken, generateAccessToken, tokenExpired} from '../src/utils/utils'
-import jwtDecode from "jwt-decode";
-import Dao from "../src/database/dao"
-import {IUserInfo} from "../src/interfaces/interfaces";
-import {authenticateUser, getUserInfo} from "../src/controllers/auth";
+import Dao from '../src/database/dao'
+import { IUserInfo } from '../src/interfaces/interfaces'
 
 // jest.mock('../src/utils/utils', () => ({
 //   generateAccessToken: jest.fn(() => ({token: 1234})),
 //   decodeToken: jest.fn(() => ({token: 1234, email: "al@hotmail.com"})),
-//   tokenExpired: jest.fn().mockReturnValueOnce(true).mockReturnValueOnce(false),
-// }));
-//
+//   tokenExpired:
+// jest.fn().mockReturnValueOnce(true).mockReturnValueOnce(false), }));
 // jest.mock("../src/database/dao")
-jest.mock('jwt-decode', () => jest.fn());
+jest.mock('jwt-decode', () => jest.fn())
 jest.mock('pg')
 
-
 describe('Dao authenticate user', () => {
-
   test('dao authenticate user code 201', async () => {
     const userInfo: IUserInfo = {
       personId: 1,
-      country: "1",
-      languages: "1",
+      country: '1',
+      languages: '1',
       academicalDataId: 1,
       technicalDataId: 1,
       workDataId: 1,
       isAvailable: true,
-      softSkills: "1",
+      softSkills: '1',
       interviewId: 1,
-      email: "1",
-      password: "1",
-      token: "1",
+      email: '1',
+      password: '1',
+      token: '1',
     }
 
-    const result = {msg: "201"}
-    let dao = new Dao();
-    const expected = await dao.authenticateUser(1, "1", "1", 1, 1, 1, true, "1", 1, "1", "1", "1");
+    const result = { msg: '201' }
+    const dao = new Dao()
+    const expected = await dao.authenticateUser(
+      1,
+      '1',
+      '1',
+      1,
+      1,
+      1,
+      true,
+      '1',
+      1,
+      '1',
+      '1',
+      '1',
+    )
     expect(result).toStrictEqual(expected)
   })
 
   test('Dao get user info code 400', async () => {
-    const result = {msg: "400"}
-    let dao = new Dao();
-    const expected = await dao.getUserInfo("al@hotmail.com");
+    const result = { msg: '400' }
+    const dao = new Dao()
+    const expected = await dao.getUserInfo('al@hotmail.com')
     expect(result).toStrictEqual(expected)
   })
 })
-
-
-
-
-
-
-
