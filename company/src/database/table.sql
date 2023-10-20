1f40755639ba093a16ede39ed22143a75914a055
Luis Guzman <luis.guzman@falcani.com>
2023-10-19 23:21:42 -0500

    Upload Company table

---
+++ company/src/database/table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "Company"
+(
+    "company_name"       TEXT,
+    "email"              TEXT NOT NULL unique,
+    "main_address"       TEXT,
+    "main_contact"       TEXT,
+    "password"           TEXT NOT NULL,
+    "preferred_language" TEXT,
+    "segments"           TEXT[],
+    "size"               TEXT
+);
