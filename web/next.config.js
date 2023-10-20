de3a65bbbafb53400e90ba18b1413e6b3ca4a0a4
Luis Guzman <luis.guzman@falcani.com>
2023-10-20 12:01:44 -0500

    Add create project form tests

--- web/next.config.js
+++ web/next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     HOST: process.env.HOST,
     CANDIDATE_PORT: process.env.CANDIDATE_PORT,
     COMPANY_PORT: process.env.COMPANY_PORT,
+    PROJECT_PORT: process.env.PROJECT_PORT,
   },
 }
 
