5cfd81c737d2cf395af7cb6d85766e2d9120a79c
Luis Guzman <luis.guzman@falcani.com>
2023-10-20 07:50:30 -0500

    Add company profile form tests

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   env: {
     HOST: process.env.HOST,
     CANDIDATE_PORT: process.env.CANDIDATE_PORT,
+    COMPANY_PORT: process.env.COMPANY_PORT,
   },
 }
 
--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,1 +1,2 @@
 export const CANDIDATE_HOST = `${process.env.HOST}:${process.env.CANDIDATE_PORT}`
+export const COMPANY_HOST = `${process.env.HOST}:${process.env.COMPANY_PORT}`
