17c6a7954479dee43a11c8e81f1b41364e99d45d
Luis Guzman <luis.guzman@falcani.com>
2023-10-28 19:58:28 -0500

    Remove unneeded comment and return

--- web/src/schemas/CommaSeparatedList.ts
+++ web/src/schemas/CommaSeparatedList.ts
@@ -1,11 +1,8 @@
 import { z } from 'zod'
 
-export const commaSeparatedList = z.string().refine(
-  (value) => {
-    return /^[a-zA-Z,]+$/.test(value) // This regex checks for alphabetic characters and commas only
-  },
-  {
+export const commaSeparatedList = z
+  .string()
+  .refine((value) => /^[a-zA-Z,]+$/.test(value), {
     message:
       'Invalid format. Please use a comma-separated list of alphabetic characters.',
-  },
-)
+  })
