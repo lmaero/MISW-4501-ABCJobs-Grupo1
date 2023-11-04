161d5ba2e0826276e7bc031a307f638e70a98cc0
Alonso Cantu <a.cantu@uniandes.edu.co>
2023-11-03 22:44:07 -0600

    Return the candidateId and implement the new table TestPerformed

--- candidate/src/routes/candidate.ts
+++ candidate/src/routes/candidate.ts
@@ -7,5 +7,7 @@ candidateRouter.get('/ping', candidateControllers.ping)
 candidateRouter.post('/register', candidateControllers.register)
 candidateRouter.post('/register/profile', candidateControllers.registerProfile)
 candidateRouter.post('/search', candidateControllers.searchCandidate)
+candidateRouter.post('/test', candidateControllers.testPerformed)
+
 
 export default candidateRouter
