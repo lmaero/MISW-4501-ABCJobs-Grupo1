f8f2047690cfb9179e97721307e15a8b31cb18c7
Alonso Cantu <a.cantu@uniandes.edu.co>
2023-11-03 22:58:00 -0600

    Return the tests to the candidate

--- candidate/src/database/dao.ts
+++ candidate/src/database/dao.ts
@@ -21,6 +21,20 @@ class Dao {
     this.client.connect()
   }
 
+  async getTests() {
+    const query = `select * from "Test"`
+    try {
+      const tests = await this.client.query(query)
+      if (tests.rows.length > 0) {
+        return {msg: '201', tests: tests.rows}
+      } else {
+        return { msg: '400' }
+      }
+    } catch (err) {
+      return { msg: '400' }
+    }
+  }
+
   async storeCandidate(
     email: string,
     password: string,
@@ -38,6 +52,61 @@ class Dao {
     } catch (err) {
       return { msg: '400' }
     }
+  }
+
+  async storeTestPerformedByCandidate(candidate_id: string, test_id: number, answers: string[] ) {
+    const query = `insert into "TestPerformed"
+                    values ($1, $2, $3)
+                   `
+    try {
+      await this.client.query(query, [candidate_id, test_id, answers])
+      console.log('Test perfomed by the candidate was saved!')
+      return { msg: '200' }
+    } catch (err) {
+      console.log(err)
+    }
+    return { msg: '400' }
+  }
+
+  async searchCandidate(
+      role: string[],
+      languages: string[],
+      soft_skills: string[],
+      spoken_languages: string[],
+  ) {
+    const query = `with result as (with role (col) as (select experience,
+                                                              technical_data ->> 'roles' as programming_languages,
+                                                              soft_skills,
+                                                              languages,
+                                                              email
+                                                       from "Candidate")
+                                   select y.x ->> 'role' "position",
+                                          soft_skills,
+                                          languages as   spoken_languages,
+                                          programming_languages,
+                                          email
+                                   from role,
+                                        lateral (select jsonb_array_elements(role.col) x) y)
+                   select *
+                   from result
+                   where 1 = 1
+                      or position in ($1)
+                      or programming_languages in ($2)
+                      or soft_skills in ($3)
+                      or spoken_languages in ($4)`
+    try {
+      const res = await this.client.query(query, [
+        role,
+        languages,
+        soft_skills,
+        spoken_languages,
+      ])
+      console.log(res)
+      return { msg: '200', res }
+    } catch (err) {
+      console.log(err)
+    }
+    return { msg: '400' }
   }
 
   async updateCandidateProfile(
@@ -77,61 +146,6 @@ class Dao {
       ])
       console.log('Candidate updated!')
       return { msg: '200' }
-    } catch (err) {
-      console.log(err)
-    }
-    return { msg: '400' }
-  }
-
-  async storeTestPerformedByCandidate(candidate_id: string, test_id: number, answers: string[] ) {
-    const query = `insert into "TestPerformed"
-                    values ($1, $2, $3)
-                   `
-    try {
-      await this.client.query(query, [candidate_id, test_id, answers])
-      console.log('Test perfomed by the candidate was saved!')
-      return { msg: '200' }
-    } catch (err) {
-      console.log(err)
-    }
-    return { msg: '400' }
-  }
-
-  async searchCandidate(
-    role: string[],
-    languages: string[],
-    soft_skills: string[],
-    spoken_languages: string[],
-  ) {
-    const query = `with result as (with role (col) as (select experience,
-                                                              technical_data ->> 'roles' as programming_languages,
-                                                              soft_skills,
-                                                              languages,
-                                                              email
-                                                       from "Candidate")
-                                   select y.x ->> 'role' "position",
-                                          soft_skills,
-                                          languages as   spoken_languages,
-                                          programming_languages,
-                                          email
-                                   from role,
-                                        lateral (select jsonb_array_elements(role.col) x) y)
-                   select *
-                   from result
-                   where 1 = 1
-                      or position in ($1)
-                      or programming_languages in ($2)
-                      or soft_skills in ($3)
-                      or spoken_languages in ($4)`
-    try {
-      const res = await this.client.query(query, [
-        role,
-        languages,
-        soft_skills,
-        spoken_languages,
-      ])
-      console.log(res)
-      return { msg: '200', res }
     } catch (err) {
       console.log(err)
     }
