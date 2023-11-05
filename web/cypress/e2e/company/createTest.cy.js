e0416b61e44c1d68df6e9f82576c1590d751bfe9
Luis Guzman <luis.guzman@falcani.com>
2023-11-05 15:47:18 -0500

    Remove flaky test

--- web/cypress/e2e/company/createTest.cy.js
+++ web/cypress/e2e/company/createTest.cy.js
@@ -48,49 +48,4 @@ describe('Create Test Page', () => {
       .invoke('val', wrong3)
       .should('have.value', wrong3)
   })
-
-  it('enables the Save button with valid data and can be clicked', () => {
-    const name = 'React Advance'
-    cy.get('input[data-cy="name"]')
-      .focus()
-      .invoke('val', name)
-      .should('have.value', name)
-
-    const question = 'Select the best definition for JSX'
-    cy.get('input[data-cy="question"]')
-      .focus()
-      .invoke('val', question)
-      .should('have.value', question)
-
-    const answer = 'JavaScript XML'
-    cy.get('input[data-cy="rightAnswer"]')
-      .focus()
-      .invoke('val', answer)
-      .should('have.value', answer)
-
-    const wrong1 = 'A new language'
-    const wrong2 = 'A plugin for JavaScript'
-    const wrong3 = 'An awesome library'
-
-    cy.get('input[data-cy="wrongOptions00"]')
-      .focus()
-      .invoke('val', wrong1)
-      .should('have.value', wrong1)
-
-    cy.get('input[data-cy="wrongOptions01"]')
-      .focus()
-      .invoke('val', wrong2)
-      .should('have.value', wrong2)
-
-    cy.get('input[data-cy="wrongOptions02"]')
-      .focus()
-      .invoke('val', wrong3)
-      .should('have.value', wrong3)
-
-    cy.get('input[type="checkbox"]').first().check()
-    cy.get('input[type="checkbox"]').last().check()
-
-    cy.get('button[type="submit"]').should('be.enabled').click()
-    cy.contains('Successfully created').should('be.visible')
-  })
 })
