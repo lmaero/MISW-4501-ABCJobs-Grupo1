75c3c3d98e95fd039f7a9116e8ed5b6c37dbd678
Luis Guzman <luis.guzman@falcani.com>
2023-11-11 08:34:55 -0500

    Add datetime picker

--- web/src/app/[lang]/interviews/schedule/[id]/page.tsx
+++ web/src/app/[lang]/interviews/schedule/[id]/page.tsx
@@ -1,7 +1,36 @@
+'use client'
+
+import { useTranslations } from 'next-intl'
+import { useState } from 'react'
+import ReactDatePicker from 'react-datepicker'
+
 interface Props {
   params: { id: string }
 }
 
 export default function Page({ params }: Props) {
-  return <div>{params.id}</div>
+  const t = useTranslations('SchedulerPage')
+  const [startDate, setStartDate] = useState(new Date())
+
+  return (
+    <div className='mx-auto max-w-7xl p-12'>
+      <form className='flex flex-col items-center space-y-3'>
+        <label htmlFor='calendar'>{t('calendarLabel')}</label>
+        <ReactDatePicker
+          className='rounded-lg px-8'
+          id='calendar'
+          selected={startDate}
+          onChange={(date) => setStartDate(date || new Date())}
+          showTimeSelect
+          timeFormat='HH:mm'
+          startOpen
+          minTime={new Date('2023-11-11T08:00:00')}
+          maxTime={new Date('2023-11-11T18:00:00')}
+          timeIntervals={60}
+          timeCaption='time'
+          dateFormat='MMMM d, yyyy h:mm aa'
+        />
+      </form>
+    </div>
+  )
 }
