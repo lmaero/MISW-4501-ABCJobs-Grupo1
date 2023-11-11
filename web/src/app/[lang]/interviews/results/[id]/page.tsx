c94d9cc3a51aef6ef0c9a0e5420b966741da8308
Luis Guzman <luis.guzman@falcani.com>
2023-11-11 15:01:29 -0500

    Add results graphs

--- web/src/app/[lang]/interviews/results/[id]/page.tsx
+++ web/src/app/[lang]/interviews/results/[id]/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { useJWT } from '@/hooks/useToken'
 import { Progress } from '@mantine/core'
 import { useTranslations } from 'next-intl'
+import { v4 } from 'uuid'
 import z from 'zod'
 
 interface Props {
@@ -15,12 +17,42 @@ const InterviewResultSch = z.object({
 
 type InterviewResult = z.infer<typeof InterviewResultSch>
 
+const fakeData = [
+  { id: 1, score: 60, testName: 'React' },
+  { id: 2, score: 75, testName: 'JavaScript' },
+  { id: 3, score: 80, testName: 'Vue' },
+  { id: 4, score: 85, testName: 'Angular' },
+  { id: 5, score: 90, testName: 'Svelte' },
+  { id: 6, score: 95, testName: 'Empathy' },
+  { id: 7, score: 100, testName: 'Time management' },
+  { id: 8, score: 90, testName: 'Critical thinking' },
+  { id: 9, score: 85, testName: 'Organization' },
+  { id: 10, score: 80, testName: 'Creativity' },
+]
+
 export default function Page({ params }: Props) {
-  const t = useTranslations('SchedulerPage')
+  const t = useTranslations('InterviewResultsPage')
+  const payload = useJWT()
 
   return (
     <div className='mx-auto max-w-7xl p-12'>
-      <Progress value={50} size='xl' className='w-full' />
+      <h1 className='text-xl mb-6'>
+        <span className='font-semibold'>
+          {t('resultsFor')} {payload?.email as string}
+        </span>
+      </h1>
+
+      <div className='grid grid-cols-2 gap-y-4 gap-x-20'>
+        {fakeData.map((result) => (
+          <div key={v4()}>
+            <p>{result.testName}</p>
+            <div className='flex items-center space-x-2'>
+              <Progress value={result.score} size='xl' className='w-full' />
+              <span>{result.score}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
