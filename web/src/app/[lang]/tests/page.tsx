2b47e653342aa7fa1392280a727003b6ba03a422
Luis Guzman <luis.guzman@falcani.com>
2023-11-19 17:20:26 -0500

    Add sort by performance

--- web/src/app/[lang]/tests/page.tsx
+++ web/src/app/[lang]/tests/page.tsx
@@ -25,6 +25,7 @@ interface Result {
 export default function TestsResultsPage({ params }: Props) {
   const t = useTranslations('TestsResultsPage')
   const [results, setResults] = useState([])
+  const [order, setOrder] = useState<string>('high')
 
   useEffect(() => {
     async function getData() {
@@ -34,15 +35,16 @@ export default function TestsResultsPage({ params }: Props) {
       if (!data.results) setResults([])
       else setResults(data.results)
     }
+
     void getData()
   }, [])
 
   if (results.length === 0)
     return (
-      <div className='mx-auto max-w-7xl p-7 space-y-3'>
+      <div className='mx-auto max-w-7xl space-y-3 p-7'>
         <p className='font-semibold'>{t('notCreated')}</p>
         <Link
-          className='relative max-w-fit inline-flex items-center gap-x-1.5 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
+          className='relative inline-flex max-w-fit items-center gap-x-1.5 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
           href='/tests/create'
         >
           {t('createTest')}
@@ -53,43 +55,63 @@ export default function TestsResultsPage({ params }: Props) {
   return (
     <div className='mx-auto max-w-2xl p-8'>
       <form className='space-y-6'>
-        <header className='flex justify-between'>
-          <h2 className='mb-3 text-2xl font-bold leading-7 tracking-tight text-gray-900'>
-            {t('title')}
-          </h2>
+        <header>
+          <div className='mb-3 flex justify-between'>
+            <h2 className='mb-3 text-2xl font-bold leading-7 tracking-tight text-gray-900'>
+              {t('title')}
+            </h2>
 
-          <Link
-            className='relative inline-flex items-center gap-x-1.5 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
-            href='/tests/create'
-          >
-            {t('createTest')}
-          </Link>
+            <Link
+              className='relative inline-flex items-center gap-x-1.5 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
+              href='/tests/create'
+            >
+              {t('createTest')}
+            </Link>
+          </div>
+          <div className='space-y-2'>
+            <p>Sort by</p>
+            <select
+              className='w-full rounded-lg'
+              onChange={(event) => setOrder(event.target.value)}
+            >
+              <option value='high'>High performant</option>
+              <option value='less'>Less performant</option>
+            </select>
+          </div>
         </header>
         <hr className='border-b-1' />
         <section className='space-y-3'>
-          {results.map((result: Result) => (
-            <article
-              key={v4()}
-              className='grid capitalize grid-cols-2 rounded-md border-2 border-gray-50 p-5'
-            >
-              <div className='space-y-2'>
-                <h3 className='font-bold'>{result.candidate}</h3>
-                <p className='text-sm text-gray-400'>{result.test_type}</p>
-                <Link
-                  href={`/${params.lang}/interviews/schedule/${result.id}`}
-                  className='flex items-center text-sm text-gray-500 hover:text-gray-900 transition-all'
-                >
-                  <CalendarDaysIcon className='h-6 mr-2' />
-                  Schedule Interview
-                </Link>
-              </div>
-              <div className='space-y-0.5 justify-self-end text-right'>
-                <p className='font-medium'>{result.test_name}</p>
-                <p className='text-sm text-gray-400'>{result.result}</p>
-                <p>{result.score}%</p>
-              </div>
-            </article>
-          ))}
+          {results
+            .sort((a: Result, b: Result) => {
+              if (order === 'high') {
+                return b.score - a.score
+              } else {
+                return a.score - b.score
+              }
+            })
+            .map((result: Result) => (
+              <article
+                key={v4()}
+                className='grid grid-cols-2 rounded-md border-2 border-gray-50 p-5 capitalize'
+              >
+                <div className='space-y-2'>
+                  <h3 className='font-bold'>{result.candidate}</h3>
+                  <p className='text-sm text-gray-400'>{result.test_type}</p>
+                  <Link
+                    href={`/${params.lang}/interviews/schedule/${result.id}`}
+                    className='flex items-center text-sm text-gray-500 transition-all hover:text-gray-900'
+                  >
+                    <CalendarDaysIcon className='mr-2 h-6' />
+                    Schedule Interview
+                  </Link>
+                </div>
+                <div className='space-y-0.5 justify-self-end text-right'>
+                  <p className='font-medium'>{result.test_name}</p>
+                  <p className='text-sm text-gray-400'>{result.result}</p>
+                  <p>{result.score}%</p>
+                </div>
+              </article>
+            ))}
         </section>
       </form>
     </div>
